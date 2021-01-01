c5188da9c52ab1a16901c3cf7adc4b3c69720013
Radwan Suufi <rd.90@hotmail.com>
2021-01-01 14:19:53 +0000

    Add user View component

---
+++ Components/UserNavItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OAuthGitClientApp.Github;
+
+namespace OAuthGitClientApp.Components
+{
+    public class UserNavItem : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var userAvatarUrl = string.Empty;
+            if (User.Identity != null && User.Identity.IsAuthenticated)
+            {
+                userAvatarUrl = UserClaimsPrincipal.Claims
+                    .SingleOrDefault(cl => cl.Type == GitClaimTypes.AvatarUrl)
+                    ?.Value;
+            }
+
+            return View(model: userAvatarUrl);
+        }
+    }
+}
---
+++ Github/GitClaimTypes.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace OAuthGitClientApp.Github
+{
+    public static class GitClaimTypes
+    {
+        public const string Avatar= ClaimTypeNamespace + "/authenticationinstant";
+        public const string AuthenticationMethod = ClaimTypeNamespace + "/authenticationmethod";
+        public const string CookiePath = ClaimTypeNamespace + "/cookiepath";
+    }
+}
