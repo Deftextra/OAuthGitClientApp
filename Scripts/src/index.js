2ddd807c6f9b6ff913d7180281940da08a8a9001
Radwan Suufi <rd.90@hotmail.com>
2021-01-09 03:40:22 +0000

    Show logout button instead of profile picture for smaller screens.

--- Scripts/src/index.js
+++ Scripts/src/index.js
@@ -7,9 +7,17 @@ window.addEventListener("load", () => {
     /* Add margin to container to prevent footer from overlapping */
     const footerHeight = $("footer").outerHeight(true);
     $(".container-fluid").css("margin-bottom", footerHeight.toString() + "px" );
-
-
-
 })
 
-
+$(window).on('resize', function(){
+    var win = $(this); //this = window
+    if (win.width() <= 786) {
+        if($(".navbar:nth-child(2)").con) {
+            $(".nav-link > img").css("display", "none");
+        }
+    }
+    
+    // if (win.width() > 786) {
+    //     $(".nav-link > img").css("display", "inline-block");
+    // }
+});
