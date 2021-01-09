cc84e48bf88b98f6b14800cf5baddc9e9893f99d
Radwan Suufi <rd.90@hotmail.com>
2021-01-09 02:35:40 +0000

    Add more styling adjustment to page layout.

--- Scripts/src/index.js
+++ Scripts/src/index.js
@@ -1,5 +1,15 @@
 import 'bootstrap';
 import '../sass/index.scss'
+import $ from 'jquery'
 
+
+window.addEventListener("load", () => {
+    /* Add margin to container to prevent footer from overlapping */
+    const footerHeight = $("footer").outerHeight(true);
+    $(".container-fluid").css("margin-bottom", footerHeight.toString() + "px" );
+
+
+
+})
 
 
