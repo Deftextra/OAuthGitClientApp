930fe497d1cb39c10b06771ff6683c824d2f1f93
Radwan Suufi <rd.90@hotmail.com>
2021-01-14 21:32:14 +0000

    Add start script.

---
+++ start.sh
@@ -0,0 +1,12 @@
+## Start backend server
+echo "Starting backend server...."
+dotnet watch run
+
+## Start asset server
+if [ -d ./Scripts ];then 
+    cd Scripts 
+    echo "starting asset server..."
+    npm run start
+fi
+    
+
